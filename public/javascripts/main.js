135c66e48bfacda1b9158d83e846956f62c78edf
Justin Crites <crites.justin@gmail.com>
2014-10-02 12:50:22 -0500

    Initialize

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,4 +3,18 @@ $(document).ready(function(){
        function(){ $(this).addClass('active') },
        function(){ $(this).removeClass('active') }
 	)
+
+	var  mn = $(".main-nav");
+  mns = "main-nav-scrolled";
+  hdr = $('.orbit-container').height() - $(".main-nav").height() - $(".nav-header").height() - 10;
+
+	$(window).mousemove(function() {
+		console.log($(this).scrollTop());
+  		if( $(this).scrollTop() > hdr ) {
+  			console.log("sticky");
+    		mn.addClass(mns);
+  		} else {
+    		mn.removeClass(mns);
+  		}
+	});
 });
